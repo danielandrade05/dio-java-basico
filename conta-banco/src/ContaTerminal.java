4a4d98a035f922f38eef1287c6ab53dff1932988
Daniel de Andrade Pereira <05danielandrade@gmail.com>
2023-09-09 15:29:57 -0300

    Envio do desafio de código sobre sintaxe Java

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,26 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // criando o objeto scanner
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        // recebendo dados do usuário via scanner
+        System.out.println("Por favor, digite o número da conta!");
+        int numero = scanner.nextInt();
+
+        System.out.println("Por favor, digite a agência!");
+        String agencia = scanner.next();
+
+        System.out.println("Por favor, digite o seu nome!");
+        String nomeCliente = scanner.next();
+
+        System.out.println("Por favor, digite o seu saldo!");
+        double saldo = scanner.nextDouble();
+
+        // imprimindo resultado
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é "
+                + agencia + ", conta " + numero + " e seu saldo R$ " + saldo + " já está disponível para saque");
     }
 }
